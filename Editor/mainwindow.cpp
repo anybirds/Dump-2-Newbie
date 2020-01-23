d63d7429815020bdd82efda6ef6795f783538bcc
KimHajun <river0105@naver.com>
2020-01-23 15:45:08 +0900

    initial Editor run

--- Editor/mainwindow.cpp
+++ Editor/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <custom.hpp>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    Custom custom;
 }
 
 MainWindow::~MainWindow()
